dab120b54e75b60ad5cf236d14edbaa644fd2a89
elmurotyangiboev <elmurotyangiboev@elmurots-imac.fios-router.home>
2019-05-09 14:48:18 -0400

    added new hooks feature to Cucumber BDD framework

--- CucumberBDDFramework/src/main/java/myRunner/TestRunner.java
+++ CucumberBDDFramework/src/main/java/myRunner/TestRunner.java
@@ -7,12 +7,12 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "/Users/elmurotyangiboev/git/CucumberBDDFramework/CucumberBDDFramework/src/main/java/features/tagging.feature",//the path of feature files
+		features = "/Users/elmurotyangiboev/git/CucumberBDDFramework/CucumberBDDFramework/src/main/java/features/hooks.feature",//the path of feature files
 		glue = {"stepDefinition"}, //the path of step definition files
 		plugin = {"pretty", "html:target/cucumber", "json:json_output/cucumber.json"}, // to generate different types of reporting
 		monochrome = true, // display the console output in a proper readable format
-		dryRun = true, // to check mapping is proper between feature file and step definition file
-		tags= {"@End2End,~@SmokeTest"}
+		dryRun = false // to check mapping is proper between feature file and step definition file
+//		tags= {"@End2End,~@SmokeTest"}
 		)
 
 public class TestRunner {
