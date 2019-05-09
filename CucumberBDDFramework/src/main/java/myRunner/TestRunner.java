c9717b6cf8764be6e9027befe2aae228080ada25
elmurotyangiboev <elmurotyangiboev@elmurots-imac.fios-router.home>
2019-05-09 15:21:42 -0400

    Hooks concept were added into Cucumber BDD Framework

--- CucumberBDDFramework/src/main/java/myRunner/TestRunner.java
+++ CucumberBDDFramework/src/main/java/myRunner/TestRunner.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "/Users/elmurotyangiboev/git/CucumberBDDFramework/CucumberBDDFramework/src/main/java/features/hooks.feature",//the path of feature files
+		features = "/Users/elmurotyangiboev/git/CucumberBDDFramework/CucumberBDDFramework/src/main/java/features/taggedhooks.feature",//the path of feature files
 		glue = {"stepDefinition"}, //the path of step definition files
 		plugin = {"pretty", "html:target/cucumber", "json:json_output/cucumber.json"}, // to generate different types of reporting
 		monochrome = true, // display the console output in a proper readable format
